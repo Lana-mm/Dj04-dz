8e1c07dd8e3ff1e87207a2117aa5a7934eaa9d0e
Lana-mm <malimonovas@list.ru>
2024-12-02 18:47:30 +0500

    end1

---
+++ movie_project/films/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from .views import add_film, film_list
+
+urlpatterns = [
+    path('add/', add_film, name='add_film'),
+    path('', film_list, name='film_list'),
+]
